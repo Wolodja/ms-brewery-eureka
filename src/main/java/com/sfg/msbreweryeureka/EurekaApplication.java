aad5f8c876e72677335fa107d9bb7ff4e8884652
Wolodja <wolodjawaskovych@i.ua>
2021-03-24 18:29:10 +0100

    Initial Configuration.

--- src/main/java/com/sfg/msbreweryeureka/EurekaApplication.java
+++ src/main/java/com/sfg/msbreweryeureka/EurekaApplication.java
@@ -2,7 +2,9 @@ package com.sfg.msbreweryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
